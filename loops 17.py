9a0628c90533fcc57665fd086a25a36d356060b4
xxxlphz <144443584+xxxlphz@users.noreply.github.com>
2023-09-24 10:40:34 +0100

    Update and rename loops 17 (incomplete).py to loops 17.py

--- loops 17.py
+++ loops 17.py
@@ -2,7 +2,7 @@
 
 age = int(input("What is your age? "))
 
-if age <= 18:
+if age >= 18:
     print("you can vote!")
     
 elif age == 17:
@@ -11,5 +11,8 @@ elif age == 17:
 elif age == 16:
     print("you can buy a lottery ticket")
     
-elif age < 16:
-    print("you can go trick or treating ")
+elif age < 16 and age>2:
+    print("you can go trick or treating xD ")
+    
+elif age <3:
+    print("cap.")
